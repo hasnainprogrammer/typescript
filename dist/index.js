77c1e2b2f9afb67c1fd39befeb91e9b5c14fe0a6
hasnain <hasnainprogrammer@gmail.com>
2026-03-12 11:56:23 +0500

    feat: implement type annotations

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let userName = "John";
+let age = 30;
+let salary = 50_00_00_0;
+let canDrive = true;
+console.log(userName, age, salary, canDrive);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+let userName: string = "John";
+let age: number = 30;
+let salary: number = 50_00_00_0;
+let canDrive: boolean = true;
+
+console.log(userName, age, salary, canDrive);
